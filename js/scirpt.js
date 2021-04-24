9679ea6cfd624531f6d89e7607536eb18b407226
edwardedo13 <edwardedo603@gmail.com>
2021-04-25 04:07:30 +0700

    test-2

--- js/scirpt.js
+++ js/scirpt.js
@@ -1,9 +1,11 @@
 // toogle menu
 const selectMenu = document.getElementById ('toggle-menu');
 const showMenu = document.querySelector ('.navbar ul') ;
+const scrollFixMenu = document.querySelector ('div.menu');
 selectMenu.addEventListener('click', function(){
     showMenu.classList.toggle('show');
     document.body.classList.toggle('lock-scroll');
+    scrollFixMenu.classList.toggle('scrol-fix');
 });
 
 
