941a1a8e97daf764f1d9006e7c7205d83e16cdca
edwardedo13 <edwardedo603@gmail.com>
2021-04-27 06:10:30 +0700

    test

--- js/scirpt.js
+++ js/scirpt.js
@@ -2,11 +2,11 @@
 const selectMenu = document.getElementById('toggle-menu');
 const showMenu = document.querySelector('.navbar ul');
 const scrollFixMenu = document.querySelector('div.menu');
-selectMenu.addEventListener('click', function () {
-    showMenu.classList.toggle('show');
-    document.body.classList.toggle('lock-scroll');
-    scrollFixMenu.classList.toggle('scrol-fix');
-});
+        selectMenu.addEventListener('click', function () {
+            showMenu.classList.toggle('show');
+            document.body.classList.toggle('lock-scroll');
+            scrollFixMenu.classList.toggle('scrol-fix');
+        });
 
 
 //test Parallax Scrolling
